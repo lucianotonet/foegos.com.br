f993d70a1b4a2efd69ac05794d4e77e396e985ba
Luciano Tonet <tonet@gmail.com>
2016-05-01 15:52:45 -0300

    Integrado com face Apps

---
+++ _partials/agenda.php
@@ -0,0 +1,77 @@
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '387632808073980',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/pt_BR/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-8 col-md-offset-2 bg-dark">
+				
+					<div class="text-center">
+					  	<h1>AGENDA</h1>
+					  	<p>Fique ligado...</p>
+					</div>
+					
+
+					<div class="events">
+			
+					</div>
+					<!-- End Events posts -->
+			
+					<a href="https://www.facebook.com/foegos/events" class="btn btn-danger btn-block">VER AGENDA COMPLETA <i class="fa fa-chevron-right"></i></a>
+
+				
+			</div>
+		</div>
+
+		<hr>
+
+		<div class="row">
+			
+			<div class="col-md-6 text-right">
+			  	
+			  	<div class="fb-page" data-href="https://www.facebook.com/foegos" data-width="" data-hide-cover="false" data-show-facepile="false" data-show-posts="false"></div>
+
+			</div>	
+
+			<div class="col-md-6">
+				<div
+				  class="fb-like"
+				  data-href="https://www.facebook.com/foegos"
+				  data-share="true"
+				  data-width="300"
+				  data-height="400"
+				  data-show-faces="true"
+				  data-show-facepile="true" >
+				</div>				
+			</div>
+							
+		</div>
+		&nbsp;
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h1 class="fa-2x">GRATOS PELOS <strong><span class="fb-likes"></span></strong> LIKES!</h1>						
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 bg-dark text-center">
+				<div class="fb-comments" data-href="https://www.facebook.com/foegos" data-numposts="10" data-colorscheme="dark"></div>			
+			</div>
+		</div>								
+
+	</div>
---
+++ js/fb.js
@@ -0,0 +1,112 @@
+!function(){
+	"use strict";
+
+
+	var get_event_details = function (id) {
+			$.ajax({
+			url: 'https://graph.facebook.com/v2.3/'+id+'/feed?access_token=1094566467237112|d139094f4c03e6b38fc847c93d32b53d',
+			type: 'GET',
+			dataType: 'json',		
+		})
+		.done(function( response ) {
+			console.log( response );
+
+			// $('.event-display').html('<img src="'+response.cover.source+'">');
+			$('.event-display').html( response.message );
+
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});	
+	};
+
+	
+	
+
+	$.ajax({
+		url: 'https://graph.facebook.com/v2.3/297402363703689/events?access_token=387632808073980|b234f72d6e537ace2eae0235c6520a00&fields=id,cover,name,picture,place,description,attending,start_time',
+		type: 'GET',
+		dataType: 'json',		
+	})
+	.done(function( response ) {
+
+		// console.log(response);		
+
+		$.each(response.data.reverse(), function(index, foegos_event) {
+			var event_date 	= new Date( foegos_event.start_time );
+			var event_date 	= moment( foegos_event.start_time ).locale('pt');
+			var event_id 	= foegos_event.id;
+
+			var event_html = $.parseHTML(foegos_event.description);
+
+			console.log(event_html);
+
+			var output  =  	'<article class="event cf" data-event="'+foegos_event.id+'" data-cover="" id="'+foegos_event.id+'">';
+				output +=	'<time datetime="" class="date">'+
+								'<span class="date__day">'+event_date.format('DD')+'</span>'+
+								event_date.format('MMMM')+
+								'<span class="date__year">'+event_date.format('YYYY')+'</span>'+
+							'</time>';
+				output +=	'<div class="event_wrap">'+
+								'<div class="event_div">'+
+									'<h1 class="event_title">'+
+										'<a href="https://fb.com/'+foegos_event.id+'">'+foegos_event.name+'</a>'+
+									'</h1>';
+				if( 'place' in foegos_event && 'location' in foegos_event.place ){
+					output +=	 	'<p class="event_category">'+
+										'<i class="fa fa-map-marker"></i>'+foegos_event.place.location.city+
+									'</p>';
+				}
+				output += 		'</div>';
+				if( 'description' in foegos_event ){
+					output +=	'<p class="event_description">'+event_html+'</p>';
+					output +=	'<p class="">'+event_date.fromNow()+'</p>';
+				}else{
+					output +=	'<p class="event_description text-capitalize">'+event_date.fromNow()+'</p>';
+				}
+				output += 	'</div>'+
+							'</article>';
+			
+
+			$('.events').prepend( output );
+
+			$( '#'.event_id+' .event_description' ).html(event_html);
+
+			jQuery(".timeago").timeago();
+
+			$('article.event').click(function(event) {
+				get_event_details( $(this).data('event') );
+			}); 
+
+		});
+
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});	
+
+	// TIMEAGO
+	jQuery(".timeago").timeago();
+
+
+
+	// FACEBOOK LIKES
+	$.ajax({
+		url: 'https://graph.facebook.com/v2.3/297402363703689?access_token=387632808073980|b234f72d6e537ace2eae0235c6520a00',
+		type: 'GET',
+		dataType: 'json',		
+	})
+	.done(function( response ) {
+		$('.fb-likes').html( response.likes );
+	});
+
+	
+
+
+}();
